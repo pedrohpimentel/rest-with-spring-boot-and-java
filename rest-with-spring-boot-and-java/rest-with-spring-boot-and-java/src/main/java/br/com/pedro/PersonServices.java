eb69550405ab035bc3ff17fdad7e82cb85e01173
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2025-10-11 07:26:42 -0300

    Retornando lista de objetos em JSON

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/pedro/PersonServices.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/pedro/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.pedro;
 import br.com.pedro.service.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,15 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());//Usado para registrar informações (logs).
+
+    public List<Person> findAll(){
+       List <Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson (i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(String Id){
         logger.info("Finding one Person!"); //Registra uma mensagem no log do sistema.
@@ -23,5 +34,16 @@ public class PersonServices {
         person.setGender("Male");
         return person;
     }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Set Adress in Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
 
 }
--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/pedro/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/pedro/controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController//Marca a classe como um Controlador Rest
 @RequestMapping("/person")
 public class PersonController {
@@ -16,10 +18,21 @@ public class PersonController {
     @Autowired //Injeção de dependência do Spring
     private PersonServices service;
 
+    @RequestMapping( // Como não estou passando um parâmetro ele irá para o default,
+            //que será o findAll, se eu passar um parâmetro ele irá para o findById (SE FOR UM GET).
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll (){
+        return service.findAll();
+    }
+
     @RequestMapping(value = "/{id}",
     method = RequestMethod.GET,
-    produces = MediaType.APPLICATION_JSON_VALUE)//O produces garante que o retorno será em formato JSON.
+    produces = MediaType.APPLICATION_JSON_VALUE
+    )//O produces garante que o retorno será em formato JSON.
     public Person findById (@PathVariable("id") String id){
        return service.findById(id);
     }
+
 }
